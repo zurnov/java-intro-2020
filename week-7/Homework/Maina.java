285d7041295ab62174329821c9c065fb8de2ea6e
Dimitar Zurnov <55513656+zurnov@users.noreply.github.com>
2020-05-26 23:16:08 +0300

    Add files via upload

--- week-7/Homework/Maina.java
+++ week-7/Homework/Maina.java
@@ -1,6 +1,7 @@
 
 public class Maina {
     public static void main(String[] args) {
+        System.out.println("Use *:* to separate ne numbers. Example: 4:3");
         GameLogic gameLogic = new GameLogic();
         gameLogic.createField();
         gameLogic.loop();
